834fbd96e41e3046c1101b398ac9bc86b0196986
Gustavo Caldeira <gustavoc.lightbase@sebraemg.com.br>
2023-07-11 15:38:32 -0300

    feat: desenhada tela de perfil

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -5,14 +5,115 @@ export default function Dashboard({ auth }) {
     return (
         <AuthenticatedLayout
             user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
+            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Meus dados</h2>}
         >
-            <Head title="Dashboard" />
+            <Head title="Meu Perfil" />
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">You're logged in!</div>
+                        <div className="p-6 text-gray-900">
+                            <form>
+                                <div className="flex flex-wrap">
+                                    <div className="grid grid-cols-1 space-y-2">
+                                        <label className="text-sm font-bold text-gray-500 tracking-wide">Attach Document</label>
+                                        <div className="flex items-center justify-center w-full">
+                                            <label className="flex flex-col rounded-lg border-4 border-dashed w-full h-60 p-10 group text-center">
+                                                <div className="h-full w-full text-center flex flex-col items-center justify-center items-center  ">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" className="w-10 h-10 text-blue-400 group-hover:text-blue-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
+                                                    </svg>
+                                                    <div className="flex flex-auto max-h-48 w-full mx-auto -mt-10">
+                                                        <img style={{width:'9rem',height:'6rem'}} className="rounded" src="https://img.freepik.com/fotos-gratis/mulher-sorridente-na-camisa-fazendo-selfie-no-estudio_171337-17196.jpg?w=1380&t=st=1689099818~exp=1689100418~hmac=1390f72b41b25c87c97d2b38d214074b1b6d1e45cd452667ebacb0d6e8eab669" alt="freepik image" />
+                                                    </div>
+                                                    <p className="pointer-none text-gray-500 "><span className="text-sm">Arraste</span> sua foto <br /> ou <a href="" id="" className="text-blue-600 hover:underline">Faça upload</a></p>
+                                                </div>
+                                                <input type="file" className="hidden" />
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="flex flex-wrap -mx-3 mb-6">
+                                    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                                        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+                                            Primeiro Nome
+                                        </label>
+                                        <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Isabela" />
+                                        <p className="text-red-500 text-xs italic">Nome obrigatório.</p>
+                                    </div>
+                                    <div className="w-full md:w-1/2 px-3">
+                                        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
+                                            Último Nome
+                                        </label>
+                                        <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Matusalem" />
+                                    </div>
+                                </div>
+                                <div className="flex flex-wrap -mx-3 mb-6">
+                                    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                                        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-cpf">
+                                            CPF
+                                        </label>
+                                        <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-cpf" type="text" placeholder="896.686.850-91" />
+                                    </div>
+                                    <div className="w-full md:w-1/2 px-3">
+                                        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-rg">
+                                            Rg
+                                        </label>
+                                        <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-rg" type="text" placeholder="37.315.294" />
+                                    </div>
+                                </div>
+                                <div className="flex flex-wrap -mx-3 mb-6">
+                                    <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                                        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-city">
+                                            Data batismo
+                                        </label>
+                                        <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-date" type="date" placeholder="10/12/2020" />
+                                    </div>
+                                    <div className="w-full md:w-2/3 px-3 mb-6 md:mb-0">
+                                        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+                                            Endereço com número
+                                        </label>
+                                        <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-address" type="text" placeholder="Rua das Dores, 56" />
+                                    </div>
+                                </div>
+                                <div className="flex flex-wrap -mx-3 mb-2">
+                                    <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                                        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-city">
+                                            Cidade
+                                        </label>
+                                        <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" placeholder="Vespasiano" />
+                                    </div>
+                                    <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                                        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
+                                            Estado
+                                        </label>
+                                        <div className="relative">
+                                            <select className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+                                                <option>Minas Gerais</option>
+                                                <option>Missouri</option>
+                                                <option>Texas</option>
+                                            </select>
+                                            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                                                <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                                        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-zip">
+                                            CEP
+                                        </label>
+                                        <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="text" placeholder="33200-000" />
+                                    </div>
+                                </div>
+                                <div className="flex flex-wrap -mx-3 mb-6 justify-end">
+                                    <div className="px-3">
+                                        <button className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="button">
+                                            Salvar
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
